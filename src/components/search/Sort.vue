c85f115d9f37d06281d8292300b5917f6caafaa4
Ridoy Sharif <developersridoy@gmail.com>
2023-10-28 16:16:24 +0600

    sort by components color

--- src/components/search/Sort.vue
+++ src/components/search/Sort.vue
@@ -2,13 +2,13 @@
     <div>
         <div class="flex gap-2 items-center bg-white px-2 py-1 rounded-md mr-3 mb-2">
             <p class="text-base font-normal text-black">Sort by:</p>
-            <select class="py-2 focus:outline-none cursor-pointer">
-                <option class="py-2" value="our-top-picks">Our top picks</option>
-                <option class="py-2" value="popularity">Popularity</option>
-                <option class="py-2" value="highest-first">Price: highest first</option>
-                <option class="py-2" value="lowest-first">Price: lowest first</option>
-                <option class="py-2" value="number-of-reviews">Number of reviews</option>
-                <option class="py-2" value="best-rating">Best rating</option>
+            <select class="py-2 w-9/12 focus:outline-none cursor-pointer p-5 text-black">
+                <option class="mb-2 text-black" value="our-top-picks">Our top picks</option>
+                <option class="mb-2 text-black" value="popularity">Popularity</option>
+                <option class="mb-2 text-black" value="highest-first">Price: highest first</option>
+                <option class="mb-2 text-black" value="lowest-first">Price: lowest first</option>
+                <option class="mb-2 text-black" value="number-of-reviews">Number of reviews</option>
+                <option class="mb-2 text-black" value="best-rating">Best rating</option>
             </select>
         </div>
     </div>
